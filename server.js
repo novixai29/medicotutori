b6d7ccd73f164da3a0d589510dd452a4261bbab4
novixai29 <novixai29@gmail.com>
2026-04-18 01:39:23 +0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = "novix";
+
+self.addEventListener("install", e=>{
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(cache=>{
+            return cache.addAll([
+                "/",
+                "/index.html",
+                "/style.css",
+                "/script.js"
+            ]);
+        })
+    );
+});
+
+self.addEventListener("fetch", e=>{
+    e.respondWith(
+        caches.match(e.request).then(res=>res || fetch(e.request))
+    );
+});
